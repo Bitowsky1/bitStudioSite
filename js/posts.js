03ac8c280435712e43df85f96dd31a8428ef3cd6
Bitowsky2 <67092848+Bitowsky@users.noreply.github.com>
2025-01-15 13:57:11 +0100

    mainPage

---
+++ js/posts.js
@@ -0,0 +1,21 @@
+//alert(posts[0])
+
+content = document.getElementById("content")
+
+//document.getElementById("missingData").innerHTML = "<script src='" + posts[0].file + "'></script>"
+
+
+
+for (i = 0; i < 5; i++) {
+    content.innerHTML += "<div class='postBox'><div class='post postStart');'><img src='../img/post_top.png'></div><div class='post postMid' id='postId" + i + "'></div><div class='post postEnd');'><img src='../img/post_bottom.png'></div></div>"    
+    loadPost(i)
+}
+
+function loadPost(id) {
+    $(function(){
+        //alert(id)
+        $("#postId" + id).load("../posts/post_1.html"); 
+      });
+}
+
+//</tr><tr><td></td><td>" + posts[0].title + "</td></tr><br>
---
+++ posts/_allPosts.js
@@ -0,0 +1,7 @@
+var posts = new Array();
+
+posts[0] = {id: 0, title: "Post Testing - Title", file: "post_1.html"}
+posts[1] = {id: 1, title: "Post Testing - Title", file: "post_1.html"}
+posts[2] = {id: 2, title: "Post Testing - Title", file: "post_1.html"}
+posts[3] = {id: 3, title: "Post Testing - Title", file: "post_1.html"}
+posts[4] = {id: 4, title: "Post Testing - Title", file: "post_1.html"}
